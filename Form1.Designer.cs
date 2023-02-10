9392d83ba593a0793408a9bdade40f96b66ee0cb
kchuck <89504386+kchuck@users.noreply.github.com>
2023-02-10 15:04:52 -0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace KCHam
+{
+    partial class Form1
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(21, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 49);
+            this.textBox1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Forward";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Reverse";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 66);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(82, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Bidirectional";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(112, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(88, 86);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Direction";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton8);
+            this.groupBox2.Controls.Add(this.radioButton7);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(206, 90);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(102, 142);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Command Mode";
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(6, 115);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(89, 17);
+            this.radioButton8.TabIndex = 4;
+            this.radioButton8.Text = "Disable Serial";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(6, 92);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(87, 17);
+            this.radioButton7.TabIndex = 3;
+            this.radioButton7.Text = "Enable Serial";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 68);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(53, 17);
+            this.radioButton6.TabIndex = 2;
+            this.radioButton6.Text = "Home";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 43);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(66, 17);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.Text = "Calibrate";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 20);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(58, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Normal";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Response";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3.5",
+            "7.0",
+            "10.0",
+            "14.0",
+            "18.0",
+            "21.0",
+            "24.5",
+            "28.0",
+            "28.5",
+            "29.0",
+            "29.5",
+            "50.0",
+            "50.5",
+            "51.0",
+            "51.5",
+            "52.0",
+            "52.5",
+            "53.0",
+            "53.5"});
+            this.comboBox1.Location = new System.Drawing.Point(21, 106);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(52, 21);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "50",
+            "100",
+            "150",
+            "200",
+            "250",
+            "300",
+            "350",
+            "400",
+            "450",
+            "500"});
+            this.comboBox2.Location = new System.Drawing.Point(21, 155);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(52, 21);
+            this.comboBox2.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Frequency MHz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Freq Offset kHz";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 29);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 234);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "W6XB StepIR Controller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace KCHam
+{
+    public partial class Form1 : Form
+    {
+        SerialPort rsPort = new SerialPort();
+        byte[] txBuffer = new byte[11] { 0x40, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0d };
+        byte[] rxBuffer = new byte[11];
+        double freq;
+        uint count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //initialize RS-232 serial port
+            rsPort.PortName = "COM1";
+            rsPort.BaudRate = 9600;
+            rsPort.Parity = Parity.None;
+            rsPort.DataBits = 8;
+            rsPort.StopBits = StopBits.One;
+            rsPort.Handshake = Handshake.None;
+            rsPort.ReadTimeout = 2000;
+            rsPort.WriteTimeout = 500;
+            rsPort.Open();
+            //Raise RTS
+            rsPort.RtsEnable = true;
+        }
+
+        private void ReadDirection()
+        {
+            if(radioButton1.Checked)
+                txBuffer[7] = 0x00;//forward
+            else if(radioButton2.Checked)
+                txBuffer[7] = 0x40;//reverse
+            else
+                txBuffer[7] = 0x80;//bidir
+        }
+
+        private void ReadMode()
+        {
+            if (radioButton4.Checked)
+                txBuffer[8] = 0x01;//normal
+            else if(radioButton5.Checked)
+                txBuffer[8] = 0x56;//calibrate
+            else if (radioButton6.Checked)
+                txBuffer[8] = 0x53;//home
+            else if (radioButton7.Checked)
+                txBuffer[8] = 0x52;//enable serial
+            else
+                txBuffer[8] = 0x55;//disable serial
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //clear rxBuffer
+            Array.Clear(rxBuffer, 0, 11);
+            //flush rx uart
+            rsPort.DiscardInBuffer();
+            //clear text box
+            textBox1.Clear();
+
+            //query message
+            byte[] xBuffer = new byte[3] { 0x3f, 0x41, 0x0d };
+            //write serial data
+            rsPort.Write(xBuffer, 0, 3);
+
+            //receive response and print to textBox1
+            try
+            {
+                //read serial data
+                int j = 0;
+                while (j < rxBuffer.Length)
+                {
+                    rxBuffer[j] = (byte)rsPort.ReadByte();
+                    textBox1.AppendText("0x" + rxBuffer[j].ToString("X") + " ");
+                    j++;
+                }
+            }
+            catch (TimeoutException) { return; }
+
+            //convert bytes to freq
+            freq = 10 * (65536 *rxBuffer[3] + 256 * rxBuffer[4] + rxBuffer[5]);
+            double fMHz = freq / 1e6;
+            textBox1.AppendText(fMHz.ToString("F2") + " MHz");
+
+            //color light green if still, yellow if moving
+            if (rxBuffer[6] != 0)
+                textBox1.BackColor = System.Drawing.Color.Yellow;
+            else
+                textBox1.BackColor = System.Drawing.Color.LightGreen;
+
+            //forward
+            if (rxBuffer[7] == 0x15)
+                radioButton1.BackColor= System.Drawing.Color.LightGreen;
+            else
+                radioButton1.BackColor = System.Drawing.Color.Transparent;
+
+            //reverse
+            if (rxBuffer[7] == 0x55)
+                radioButton2.BackColor = System.Drawing.Color.LightGreen;
+            else
+                radioButton2.BackColor = System.Drawing.Color.Transparent;
+
+            //bidir
+            if (rxBuffer[7] == 0x95)
+                radioButton3.BackColor = System.Drawing.Color.LightGreen;
+            else
+                radioButton3.BackColor = System.Drawing.Color.Transparent;
+
+            count++;
+
+            if (count == 1)
+            {
+                //determine base frequency
+                freq /= 1e6;
+           
+                if (freq < 10.0)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("7.0");
+                else if (freq < 14.0)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("10.0");
+                else if (freq < 18.0)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("14.0");
+                else if (freq < 24.5)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("18.0");
+                else if (freq < 28.0)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("24.5");
+                else if (freq < 28.5)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("28.0");
+                else if (freq < 29.0)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("28.5");
+                else if (freq < 29.5)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("29.0");
+                else if (freq < 50.0)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("29.5");
+                else if (freq < 50.5)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("50.0");
+                else if (freq < 51.0)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("50.5");
+                else if (freq < 51.5)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("51.0");
+                else if (freq < 52.0)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("51.5");
+                else if (freq < 52.5)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("52.0");
+                else if (freq < 53.0)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("52.5");
+                else if (freq < 53.5)
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("53.0");
+                else
+                    comboBox1.SelectedIndex = comboBox1.FindStringExact("53.5");
+
+                //determine freq offset
+                double freqOffset = Math.Round((freq % 0.5) / .025) * 25;
+                comboBox2.SelectedIndex = comboBox2.FindStringExact(freqOffset.ToString());
+
+                //align to combo box freq
+                button1_Click(sender, e);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //convert combo boxes to freq
+            freq = 1e6 * (Convert.ToDouble(comboBox1.Text) + Convert.ToDouble(comboBox2.Text) / 1e3);
+            //pull out bytes of freq
+            txBuffer[3] = (byte)((((uint)freq/10)>>16) & 0xff) ;
+            txBuffer[4] = (byte)((((uint)freq/10) >> 8) & 0xff);
+            txBuffer[5] = (byte)((uint)freq/10 & 0xff);
+            ReadDirection();
+            ReadMode();
+            //write serial data
+            rsPort.Write(txBuffer, 0, txBuffer.Length);
+        }
+    }
+}   
